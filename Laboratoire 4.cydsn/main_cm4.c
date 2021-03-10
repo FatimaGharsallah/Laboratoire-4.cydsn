4300e2c7dc03be3ba0c33dfa8b802fa53807854c
kerusami <fatima.zahra@live.ca>
2021-03-10 17:37:08 -0500

    Partie 1

--- Laboratoire 4.cydsn/main_cm4.c
+++ Laboratoire 4.cydsn/main_cm4.c
@@ -16,14 +16,24 @@
 #include "params.h"
 #include "queue.h"
 
-//patatebfegjgkjeir
+void vLEDTask(void)
+{
+    for(;;)
+    {
+    Cy_GPIO_Write(LED_0_PORT,LED_0_NUM,1);
+    vTaskDelay(pdMS_TO_TICKS(250));
+    Cy_GPIO_Write(LED_0_PORT,LED_0_NUM,0);
+    vTaskDelay(pdMS_TO_TICKS(250));
+    }
+}
 
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    xTaskCreate(vLEDTask,"LED",80,NULL,1,NULL); 
+    vTaskStartScheduler();
     for(;;)
     {
         /* Place your application code here. */
